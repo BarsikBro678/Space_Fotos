2f2bcb548e555b717a3f5902038e3c2029dcf333
BarsikBro678 <112183697+BarsikBro678@users.noreply.github.com>
2023-04-22 11:19:44 +0300

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -16,12 +16,13 @@ def randomize():
 
 	
 def main():
+	time = os.environ["TIME"]
 	bot_token = os.environ["BOT_TOKEN"]
 	chat_id = os.environ["CHAT_ID"]
 	bot = telegram.Bot(token=bot_token)
 	while True:
 		telegram_bot(bot, chat_id)
-		sleep(14400)
+		sleep(time)
 		
 
 if __name__ == "__main__":

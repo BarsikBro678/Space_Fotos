c04818bf8374049ec80186926a482fdbf5fea6f5
BarsikBro678 <112183697+BarsikBro678@users.noreply.github.com>
2023-04-22 11:16:55 +0300

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -1,1 +1,28 @@
+import os
+import random
+from time import sleep
 
+import telegram 
+
+
+def telegram_bot(bot, chat_id):
+	with open(randomize(), "rb") as photo:
+		bot.send_photo(chat_id = chat_id, photo = photo)	
+
+
+def randomize():
+	images = os.listdir("images")
+	return f"images/{random.choice(images)}"
+
+	
+def main():
+	bot_token = os.environ["BOT_TOKEN"]
+	chat_id = os.environ["CHAT_ID"]
+	bot = telegram.Bot(token=bot_token)
+	while True:
+		telegram_bot(bot, chat_id)
+		sleep(14400)
+		
+
+if __name__ == "__main__":
+	main()

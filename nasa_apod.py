a9407ef767046f9756d0ad587ed6027f89ccfb01
BarsikBro678 <112183697+BarsikBro678@users.noreply.github.com>
2023-04-30 20:06:31 +0300

    Update nasa_apod.py

--- nasa_apod.py
+++ nasa_apod.py
@@ -5,11 +5,11 @@ from requests import get
 from tools import fetch_extection, load_image
 
 
-def fetch_nasa_apod(nasa_api_key, downdlaod_images=30):
+def fetch_nasa_apod(nasa_api_key, images_quantitiy=30):
 	url = "https://api.nasa.gov/planetary/apod"
 	payload = {
 		"api_key": nasa_api_key,
-		"count": download_images,
+		"count": images_quantity,
 	}
 	response = get(url, params=payload)
 	response.raise_for_status()

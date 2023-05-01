803ceea54b0b0c7476257c1e0446ed11bccf008f
BarsikBro678 <112183697+BarsikBro678@users.noreply.github.com>
2023-05-01 11:09:14 +0300

    Update tools.py

--- tools.py
+++ tools.py
@@ -5,12 +5,12 @@ from requests import get
 
 
 def load_image(image_url, image_pass, params=None):
-	response = get(image_url, params=params)
-	response.raise_for_status()
+    response = get(image_url, params=params)
+    response.raise_for_status()
 
-	with open(image_pass, 'wb') as file:
-		file.write(response.content)
+    with open(image_pass, 'wb') as file:
+        file.write(response.content)
 
 
 def fetch_extection(url):
-	return os.path.splitext(urlparse(url).path)[1]
+    return os.path.splitext(urlparse(url).path)[1]

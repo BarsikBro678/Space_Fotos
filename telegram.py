8f726e218acfb6c9ae7f9fc7136a21f74151cd5a
BarsikBro678 <112183697+BarsikBro678@users.noreply.github.com>
2023-05-01 11:05:05 +0300

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -6,24 +6,24 @@ import telegram
 
 
 def send_photo_telegram_bot(bot, chat_id):
-	with open(get_random_picture(), "rb") as photo:
-		bot.send_photo(chat_id = chat_id, photo = photo)	
+    with open(get_random_picture(), "rb") as photo:
+	bot.send_photo(chat_id = chat_id, photo = photo)	
 
 
 def get_random_picture():
-	images = os.listdir("images")
-	return f"images/{random.choice(images)}"
+    images = os.listdir("images")
+    return f"images/{random.choice(images)}"
 
 	
 def main():
-	bot_wait = os.environ["BOT_WAIT"]
-	bot_telegram_token = os.environ["BOT_TELEGRAM_TOKEN"]
-	tg_chat_id = os.environ["TG_CHAT_ID"]
-	bot = telegram.Bot(token=bot_telegram_token)
-	while True:
-		send_photo_telegram_bot(bot, tg_chat_id)
-		sleep(bot_wait)
+    bot_wait = os.environ["BOT_WAIT"]
+    bot_telegram_token = os.environ["BOT_TELEGRAM_TOKEN"]
+    tg_chat_id = os.environ["TG_CHAT_ID"]
+    bot = telegram.Bot(token=bot_telegram_token)
+    while True:
+        send_photo_telegram_bot(bot, tg_chat_id)
+        sleep(bot_wait)
 		
 
 if __name__ == "__main__":
-	main()
+    main()
